e221874b5a6a606ed22493df272f745a397fb73f
Chris Dos Santos <dossantos.chris13@gmail.com>
2023-10-30 16:43:24 -0400

    Reformatted FoodForm to be its own component

---
+++ ai-cookbook/src/components/FoodForm.tsx
@@ -0,0 +1,33 @@
+'use client'
+import { useState } from "react";
+
+const FoodForm = () => {
+    const [food, setFood] = useState("");
+
+    const handleFoodSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log(food);
+    }
+    
+    return (
+    <div className="w-1/3 bg-white p-6 rounded-lg">
+        <h1 className="text-2xl font-bold mb-4">Generate a Recipe</h1>
+        <form className="space-y-4" onSubmit={handleFoodSubmit}>
+          <div>
+            <label className="block text-sm font-medium text-gray-700">What would you like to cook?</label>
+            <input
+              className="mt-1 p-2 block w-full border rounded-md"
+              placeholder="Enter food here"
+              value={food}
+              onChange={(e) => setFood(e.target.value)}
+            />
+          </div>
+          <div>
+            <button className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600">Submit</button>
+          </div>
+        </form> 
+      </div>
+  )
+}
+
+export default FoodForm
