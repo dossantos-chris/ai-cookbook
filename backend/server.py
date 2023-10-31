46ef8be481afd1ce70cc62d6951811a33faabc16
Chris Dos Santos <dossantos.chris13@gmail.com>
2023-10-31 17:46:51 -0400

    setup dotenv

--- backend/server.py
+++ backend/server.py
@@ -1,19 +1,20 @@
 from flask import Flask, request, jsonify
 from bardapi import Bard
 import os
+from dotenv import load_dotenv, dotenv_values
+load_dotenv()
 
 app = Flask(__name__)
-bard = Bard(token="xxxxxx")
-#res = bard.get_answer("Write me a basic recipe for chocolate chip chookies")['content']
+bard = Bard(token=os.getenv("API_KEY"))
 
+#curl -X POST -H "Content-Type: text/plain" -d "chocolate cake" http://localhost:5000/api
 @app.route("/api", methods=["POST"])
 def api():
-    data = request.data   # Extract the JSON data from the request
-    if data is not None:
-        res = bard.get_answer(f"Write me a basic recipe for {data}")['content']
+    if request.data:
+        res = bard.get_answer(f"Write me a basic recipe for {request.data}")['content']
         return jsonify({'text': f'{res}'})
     else:
-        return jsonify({'error': 'No JSON data found in the request.'}), 400
+        return jsonify({'error': 'No text data found in the request.'}), 400
 
 if __name__ == "__main__":
     app.run(debug=True)
