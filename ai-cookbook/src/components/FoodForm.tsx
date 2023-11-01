ceea5e7c6afb629b2139a52a07073617544ebbda
Chris Dos Santos <dossantos.chris13@gmail.com>
2023-10-31 20:37:53 -0400

    Added error handling for fetchRecipe

--- ai-cookbook/src/components/FoodForm.tsx
+++ ai-cookbook/src/components/FoodForm.tsx
@@ -2,14 +2,15 @@
 import { useState } from "react";
 
 const FoodForm = () => {
-    const [food, setFood] = useState("");
-    const [recipe, setRecipe] = useState("");
+  const [food, setFood] = useState("");
+  const [recipe, setRecipe] = useState("");
 
-    const handleFoodSubmit = (e: React.FormEvent) => {
-      e.preventDefault();
-      
-      const fetchRecipe = async () => {
-        setRecipe("loading...")
+  const handleFoodSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    
+    const fetchRecipe = async () => {
+      setRecipe("loading...")
+      try {
         const res = await fetch("http://127.0.0.1:5000/api", {
           method: "POST",
           headers: {
@@ -17,37 +18,43 @@ const FoodForm = () => {
           },
           body: food
         });
+      
         const resData = await res.json();
-        setRecipe(resData["text"])
+        setRecipe(resData["text"]);
       }
-
-      fetchRecipe()
+      catch(e: any){
+        console.log(e);
+        setRecipe('error');
+      }
     }
 
-    return (
-    <>
-      <div className="w-1/3 bg-white p-6 rounded-lg">
-        <h1 className="text-2xl font-bold mb-4">Generate a Recipe</h1>
-        <form className="space-y-4" onSubmit={handleFoodSubmit}>
-          <div>
-            <label className="block text-sm font-medium text-gray-700">What would you like to cook?</label>
-            <input
-              className="mt-1 p-2 block w-full border rounded-md"
-              placeholder="Enter food here"
-              value={food}
-              onChange={(e) => setFood(e.target.value)}
-            />
-          </div>
-          <div>
-            <button className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600">Submit</button>
-          </div>
-        </form>
-      </div>
-      <div className="w-1/2 bg-white p-6 mt-6 rounded-lg">
-        {recipe}
-      </div>
-    </>
-    )
+    fetchRecipe()
+  }
+
+  return (
+  <>
+    <div className="w-1/3 bg-white p-6 rounded-lg">
+      <h1 className="text-2xl font-bold mb-4">Generate a Recipe</h1>
+      <form className="space-y-4" onSubmit={handleFoodSubmit}>
+        <div>
+          <label className="block text-sm font-medium text-gray-700">What would you like to cook?</label>
+          <input
+            className="mt-1 p-2 block w-full border rounded-md"
+            placeholder="Enter food here"
+            value={food}
+            onChange={(e) => setFood(e.target.value)}
+          />
+        </div>
+        <div>
+          <button className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600">Submit</button>
+        </div>
+      </form>
+    </div>
+    <div className="w-1/2 bg-white p-6 mt-6 rounded-lg">
+      {recipe}
+    </div>
+  </>
+  )
 }
 
 export default FoodForm
